ac9da4a9d351135f6f9a910c895908b9e9eab27f
Axbidaa <nezumrashid@gmail.com>
2024-03-16 21:14:10 +0400

    Finalized animations

--- login.js
+++ login.js
@@ -15,10 +15,8 @@ loginLink.addEventListener('click', ()=> {
 
 buttonPopup.addEventListener('click', ()=> {
     wrapper.classList.add('active-popup');
-    home.classList.add('active-popup');
 });
 
 iconClose.addEventListener('click', ()=> {
     wrapper.classList.remove('active-popup');
-    home.classList.remove('active-popup');
 });
